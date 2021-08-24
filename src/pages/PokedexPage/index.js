097cfa389b583fd30c476b8cf380101a1336d04c
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-08-24 12:48:40 -0400

    router

---
+++ src/pages/PokedexPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+ const PokedexPage = () => {
+
+    return (
+        <div>
+            <Header />
+            <h1>Pokedex</h1>
+        </div>
+    )   
+ }
+
+ export default PokedexPage
