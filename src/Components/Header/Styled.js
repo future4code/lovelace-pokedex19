d961a055ee6eaf486ccfcd1e87e48fb1b7a255cf
Beatriz <anabdemorais@gmail.com>
2021-08-24 02:32:05 -0300

    header

---
+++ src/Components/Header/Styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;    
+  width: auto;
+  height: 100px;
+  background-color: #0000FF;
+  block-size: fit-content
+`
+export const Button = styled.button`
+  font-size: 15px;
+  font-family: sans-serif;
+  color: #8B0000;
+  padding: 0.5em 1em;
+  outline: none;
+  margin-right: 50px;
+  background-color: #F1362F;
+  overflow: hidden;
+  block-size: fit-content
+
+`
+
+export const LogoPokemon = styled.img`
+    width: 200px;
+    margin-left: 50px;
+    block-size: fit-content
+`
